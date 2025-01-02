bb74eb20452399ced2a78acc927953f88ba2fc7e
Kacper Paczos <kacper.paczos@linux.pl>
2025-01-02 19:25:31 +0100

    I have restructured project

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,12 +14,11 @@ echo "Configuring PAM..."
 sudo cp ../config/aegis-auth /etc/pam.d/
 sudo ln -sf /etc/pam.d/aegis-auth /etc/pam.d/common-auth
 
-# Uruchomienie agenta
+# Konfiguracja i uruchomienie agenta
 echo "Starting Aegis PAM agent..."
+sudo cp ../config/aegis_pam_agent.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable aegis_pam_agent
 sudo systemctl start aegis_pam_agent
-
-sudo cp config/aegis-agent.service /etc/systemd/system/
-sudo systemctl daemon-reload
 
 echo "Installation completed successfully!" 
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -8,14 +8,15 @@ sudo systemctl stop aegis_pam_agent
 sudo systemctl disable aegis_pam_agent
 
 # Usunięcie jednostki systemd
-sudo rm -f /etc/systemd/system/aegis-agent.service
+sudo rm -f /etc/systemd/system/aegis_pam_agent.service
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
 
 # Usunięcie plików PAM
 sudo rm -f /lib/security/pam_aegis.so
 sudo rm -f /etc/pam.d/aegis-auth
-sudo rm -f /usr/local/bin/aegis-agent
+sudo rm -f /usr/local/bin/aegis_pam_agent
+sudo rm -f /etc/aegis/aegis_pam_agent.conf
 
 # Przywrócenie oryginalnej konfiguracji PAM
 sudo ln -sf /etc/pam.d/common-auth.orig /etc/pam.d/common-auth
